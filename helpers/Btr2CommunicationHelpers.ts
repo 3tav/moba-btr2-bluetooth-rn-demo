18be39fc8b58c6aae70c973b15f84c2db614dbff
Domen Bukovac <domen@bukovac.si>
2023-09-13 13:56:17 +0200

    refactored code to separate files + improved error handling

---
+++ helpers/Btr2CommunicationHelpers.ts
@@ -0,0 +1,221 @@
+import {BleCharacteristic, DISCOVER_SERVICES_DELAY, READ_WAIT_DELAY, SERVICES} from "../constants/Btr2Constants";
+import {Device} from "react-native-ble-plx";
+import {Buffer} from "buffer";
+import {sleep} from "./sleep";
+import {consoleLogBlePlxObject, discoverServicesAndCharacteristics} from "./BlePlxHelpers";
+import {getDataWithoutHeadersAndCRC, prepareResponseBuffer} from "./Btr2DataHelpers";
+import {calculateCRC32OnReceivedBtr2Data, parseCRC32FromBtr2Data} from "./crc";
+
+export const verifyAllCharacteristicsAreDiscovered = (
+  discoveredCharacteristics: BleCharacteristic[]
+): boolean => {
+  const requiredCharacteristics = [
+    SERVICES.MOBA_BTR2.CHARACTERISTICS.READ_CONTROL_POINT_32,
+    SERVICES.MOBA_BTR2.CHARACTERISTICS.READ_OBJECT_34,
+    SERVICES.MOBA_BTR2.CHARACTERISTICS.WRITE_CONTROL_POINT_36,
+    SERVICES.MOBA_BTR2.CHARACTERISTICS.WRITE_OBJECT_38,
+    SERVICES.MOBA_BTR2.CHARACTERISTICS.BONDING_CHARACTERISTIC,
+  ];
+  const discoveredCharacteristicsUUIDs = discoveredCharacteristics.map((c) => c.characteristicUUID);
+  const missingCharacteristics = requiredCharacteristics.filter((c) => !discoveredCharacteristicsUUIDs.includes(c));
+  if (missingCharacteristics.length > 0) {
+    console.log("missing characteristics: ", missingCharacteristics);
+    return false;
+  }
+  return true;
+}
+
+export const establishBtr2Bond = async (device: Device): Promise<void> => {
+  console.log("establishing pairing...");
+  try {
+    await device.readCharacteristicForService(
+      SERVICES.MOBA_BTR2.SERVICE_UUID,
+      SERVICES.MOBA_BTR2.CHARACTERISTICS.BONDING_CHARACTERISTIC,
+    );
+  } catch (e) {
+    return
+  }
+}
+
+export const connectToBtr2Device = async (device: Device, skipBonding?: boolean): Promise<Device> => {
+  // print device advertisement data
+  const advertisementData = Buffer.from(device.manufacturerData || '', 'base64').toString('utf-8');
+  console.log(`xxx${advertisementData}xxx`);
+
+  try {
+    const connectedDevice = await device.connect({
+        timeout: 5000,
+        autoConnect: false,
+      }
+    );
+
+    await sleep(DISCOVER_SERVICES_DELAY);
+    const discoveredCharacteristics = await discoverServicesAndCharacteristics(connectedDevice);
+    if (!verifyAllCharacteristicsAreDiscovered(discoveredCharacteristics)) {
+      console.log("Some required characteristics weren't discovered!");
+      return Promise.reject(Error('Some required characteristics weren\'t discovered!'));
+    }
+    await sleep(3000);
+
+    // TODO find out if device is in pairing mode
+    // if so, establish bond, else skip
+    // if not skipped when not in pairing mode, connection will fail
+
+    if (!skipBonding) {
+      await establishBtr2Bond(connectedDevice);
+      await sleep(8000);
+      return await connectToBtr2Device(device, true);
+      // await connectToDevice(connectedDevice, true)
+    } else {
+      // setConnectedDevice(connectedDevice);
+      console.log(`Connected to ${connectedDevice.name} (${connectedDevice.id})`)
+      return Promise.resolve(connectedDevice);
+    }
+  } catch (e) {
+    return Promise.reject(Error('Could not connect to the device'));
+  }
+};
+
+export const acknowledgeDataRead = async (device?: Device, data?: string): Promise<boolean> => {
+  if (!device || !data) {
+    console.log("device or data not set");
+    return Promise.reject(new Error("device or data not set"));
+  }
+
+  let responseBuffer = prepareResponseBuffer(data);
+  await sleep(READ_WAIT_DELAY);
+
+  // split data to 20 byte chunks
+  const chunkSize = 20;
+  const dataLength = responseBuffer.length;
+
+  console.log("response data length", dataLength);
+
+
+  try {
+    for (let i = 0; i < dataLength; i += chunkSize) {  // i is pointer index
+      const chunkToSend = responseBuffer.slice(i, Math.min(i + chunkSize, dataLength));
+
+      console.log(`sending chunk ${i}/${dataLength}`)
+
+      // write to handle 38, length and index
+      await device.writeCharacteristicWithResponseForService(
+        SERVICES.MOBA_BTR2.SERVICE_UUID,
+        SERVICES.MOBA_BTR2.CHARACTERISTICS.WRITE_CONTROL_POINT_36,
+        Buffer.from([dataLength, i]).toString('base64'),
+      )
+
+      // write current chunk to handle 38 (write object)
+      await device.writeCharacteristicWithResponseForService(
+        SERVICES.MOBA_BTR2.SERVICE_UUID,
+        SERVICES.MOBA_BTR2.CHARACTERISTICS.WRITE_OBJECT_38,
+        Buffer.from(chunkToSend).toString('base64'),
+      )
+      console.log(`wrote chunk ${i}/${dataLength} to handle 38`)
+    }
+
+    await device.writeCharacteristicWithResponseForService(
+      SERVICES.MOBA_BTR2.SERVICE_UUID,
+      SERVICES.MOBA_BTR2.CHARACTERISTICS.WRITE_CONTROL_POINT_36,
+      Buffer.from([dataLength, dataLength]).toString('base64'),
+    )
+    return Promise.resolve(true);
+  } catch (e) {
+    console.log("error acknowledging data read", e)
+    return Promise.reject(new Error("error acknowledging data read"));
+  }
+}
+
+export const readDataFromBTR2 = async (device?: Device): Promise<string> => {
+  if (!device) {
+    return Promise.reject(new Error("device not set"));
+  }
+  // read from handle 32 (read control point to get length)
+  let readLenCharacteristics;
+  try {
+    readLenCharacteristics = await device.readCharacteristicForService(
+      SERVICES.MOBA_BTR2.SERVICE_UUID,
+      SERVICES.MOBA_BTR2.CHARACTERISTICS.READ_CONTROL_POINT_32,
+    );
+  } catch (e) {
+    return Promise.reject(e);
+  }
+  consoleLogBlePlxObject(readLenCharacteristics, 'read control point');
+
+  // get len from base64
+  const len = Buffer.from(readLenCharacteristics.value || '', 'base64').readUInt8(0);
+  const readPointer = Buffer.from(readLenCharacteristics.value || '', 'base64').readUInt8(1);
+
+  if (len === 0) {
+    return Promise.reject(new Error("No data left to read"));
+  }
+
+  let responseLen = readPointer;
+  console.log(`len: ${len}, readPointer: ${readPointer}`)
+
+  await device.writeCharacteristicWithResponseForService(
+    SERVICES.MOBA_BTR2.SERVICE_UUID,
+    SERVICES.MOBA_BTR2.CHARACTERISTICS.READ_CONTROL_POINT_32,
+    Buffer.from([len, 0]).toString('base64'),
+  )
+
+  // read form handle 34 (read object)
+  const readObjectCharacteristics = await device.readCharacteristicForService(
+    SERVICES.MOBA_BTR2.SERVICE_UUID,
+    SERVICES.MOBA_BTR2.CHARACTERISTICS.READ_OBJECT_34,
+  );
+
+  let dataString = "";
+  let data: Buffer;
+  // decode base64 to string
+  let currentDataBuffer = Buffer.from(readObjectCharacteristics.value || '', 'base64');
+  let currentDataChunk = Buffer.from(readObjectCharacteristics.value || '', 'base64').toString('ascii');
+
+  dataString = `${dataString}${currentDataChunk}`
+  data = currentDataBuffer;
+
+  // console.log("data: ", dataString);
+  consoleLogBlePlxObject(readObjectCharacteristics, 'read object');
+
+  responseLen += data.length;
+  while (responseLen < len) {
+    await device.writeCharacteristicWithResponseForService(
+      SERVICES.MOBA_BTR2.SERVICE_UUID,
+      SERVICES.MOBA_BTR2.CHARACTERISTICS.READ_CONTROL_POINT_32,
+      Buffer.from([len, responseLen]).toString('base64'),
+    )
+    let charDataResponse = await device.readCharacteristicForService(
+      SERVICES.MOBA_BTR2.SERVICE_UUID,
+      SERVICES.MOBA_BTR2.CHARACTERISTICS.READ_OBJECT_34,
+    );
+    currentDataBuffer = Buffer.from(charDataResponse.value || '', 'base64');
+    currentDataChunk = Buffer.from(charDataResponse.value || '', 'base64').toString('ascii');
+
+    // console.log("respString: ", currentDataChunk);
+
+    dataString = `${dataString}${currentDataChunk}`
+    data = Buffer.concat([data, currentDataBuffer]);
+    responseLen = data.length;
+    console.log("responseLen: ", responseLen, 'len: ', len);
+  }
+  // console.log("data: ", dataString);
+  // console.log("data: ", data);
+  // console.log("data hex", data.toString('hex'));
+
+  const parsedCRC32 = parseCRC32FromBtr2Data(dataString);
+  console.log("parsedCRC32: ", parsedCRC32);
+  const calculatedCRC32 = calculateCRC32OnReceivedBtr2Data(data);
+
+  if (parsedCRC32 !== calculatedCRC32) {
+    console.log("CRC32 does not match");
+    return Promise.reject(new Error("CRC32 mismatch"));
+  } else {
+    try {
+      await acknowledgeDataRead(device, dataString);
+      return Promise.resolve(getDataWithoutHeadersAndCRC(data));
+    } catch (e) {
+      console.log("error acknowledging data read", e);
+      return Promise.reject(new Error("error acknowledging data read"));
+    }
+  }
+}
---
+++ helpers/Btr2DataHelpers.ts
@@ -0,0 +1,136 @@
+import {Buffer} from "buffer";
+import {crc32Calc} from "./crc";
+
+export const cleanData = (data: string) => {
+  return data.replace(/[^a-zA-Z0-9:;\],-]/g, 'X');
+}
+
+export const getDataWithoutHeadersAndCRC = (data: Buffer): string => {
+  const startByteIndex = data.indexOf('\x02') + 1;
+  const endByteIndex = data.indexOf('\x03');
+  // data with crc is between 0x02 and 0x03
+  const dataWithCrC = data.slice(startByteIndex, endByteIndex);
+  const dataBuffer = Buffer.from(dataWithCrC.slice(0, dataWithCrC.length - 8));
+  return Buffer.from(dataBuffer).toString('ascii');
+}
+
+export const getCurrentDateTimeString = (): string => {
+  // format: 2015-7-21;9:34:11
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const dayOfMonth = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const dateString = `${year}-${month}-${dayOfMonth}`;
+  const timeString = `${hours}:${minutes}:${seconds}`;
+
+  return `${dateString};${timeString}`;
+}
+
+export const prepareResponseBuffer = (data: string): Buffer => {
+  const cleanDataString = cleanData(data);
+  // split by ;
+  const dataStringArray = cleanDataString.split(';');
+  const serverMacAddress = dataStringArray[2];
+  const packetIndex = dataStringArray[3];
+
+
+  // create new buffer with length 128
+  const responseBuffer = Buffer.alloc(128);
+  let i = 0;
+
+  // write start byte 0x02
+  responseBuffer.writeUInt8(0x02, i);
+  i += 1;
+
+  // write version (string "1") to buffer
+  responseBuffer.write('1', i, 1, 'ascii');
+  i += 1;
+  // write ; to buffer
+  responseBuffer.write(';', i, 1, 'ascii');
+  i += 1;
+
+  // write kind (string "R") to buffer
+  responseBuffer.write('R', i, 1, 'ascii');
+  i += 1;
+  // write ; to buffer
+  responseBuffer.write(';', i, 1, 'ascii');
+  i += 1;
+
+  // write client mac address to buffer | does not need to be correct
+  const macAddress = "123456789ABC";
+  responseBuffer.write(macAddress, i, macAddress.length, 'ascii');
+  i += macAddress.length;
+  // write ; to buffer
+  responseBuffer.write(';', i, 1, 'ascii');
+  i += 1;
+
+  // write index to buffer
+  const dataIndex = packetIndex;
+  responseBuffer.write(dataIndex, i, dataIndex.length, 'ascii');
+  i += dataIndex.length;
+  // write ; to buffer
+  responseBuffer.write(';', i, 1, 'ascii');
+  i += 1;
+
+  // write date to buffer
+  const dateTimeString = getCurrentDateTimeString();
+  responseBuffer.write(dateTimeString, i, dateTimeString.length, 'ascii');
+  i += dateTimeString.length;
+  // write ; to buffer
+  responseBuffer.write(';', i, 1, 'ascii');
+  i += 1;
+
+  // if transponder write type, identifier, checksum and reserved with ; between, else just write ;;;;
+  // just write ;;;; for now
+  responseBuffer.write(';;;;', i, 4, 'ascii');
+  i += 4;
+
+  // if barcode write aim, identifier, reserver with ; between, else just write ;;;
+  // just write ;;; for now
+  responseBuffer.write(';;;', i, 3, 'ascii');
+  i += 3;
+
+  // if response !== null
+  // write response to buffer
+  const responseMac = serverMacAddress;
+  responseBuffer.write(responseMac, i, responseMac.length, 'ascii');
+  i += responseMac.length;
+  // write ; to buffer
+  responseBuffer.write(';', i, 1, 'ascii');
+  i += 1;
+
+  // write response index to buffer
+  const responseIndex = packetIndex
+  responseBuffer.write(responseIndex, i, responseIndex.length, 'ascii');
+  i += responseIndex.length;
+  // write ;;; to buffer
+  responseBuffer.write(';;;', i, 3, 'ascii');
+  i += 3;
+
+  // calculate CRC32
+  const crcDataBegin = 1;
+  const crcDataEnd = i;
+
+  const crc32 = crc32Calc(responseBuffer.slice(crcDataBegin, crcDataEnd));
+  console.log("crc32: ", crc32.toString(16));
+
+  // write crc as hex to buffer
+  responseBuffer.write(crc32.toString(16).toUpperCase(), i, 8, 'ascii');
+  i += 8;
+
+  // write end byte 0x03
+  responseBuffer.writeUInt8(0x03, i);
+  i += 1;
+
+  // create new buffer with length i and copy data from responseBuffer
+  const responseBufferFinal = Buffer.alloc(i);
+  responseBuffer.copy(responseBufferFinal, 0, 0, i);
+
+  // print content of response buffer as ascii
+  console.log("responseBufferFinal: ", Buffer.from(responseBufferFinal).toString('ascii'));
+  return responseBufferFinal;
+}
